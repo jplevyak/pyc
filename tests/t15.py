c9c94b2ec107c5d92bad894a546044552a7e4472
jplevyak <jplevyak@9ab72d50-4173-4535-8208-40b26c4bf95a>
2008-09-02 22:09:35 +0000

    interm
    git-svn-id: file:///home/jplevyak/projects/SVNROOT/pyc@45 9ab72d50-4173-4535-8208-40b26c4bf95a

---
+++ tests/t15.py
@@ -0,0 +1,18 @@
+class A(object):
+  n = 2
+  print n
+  def __init__(self, a):
+    print a + 10
+    print self.n + 10
+class B(A):
+  o = 3
+  print o
+  def __init__(self, a):
+    super(B, self).__init__(a)
+    print a
+    print self.n
+    print self.o
+y = B(3);
+print y.n
+print y.o
+z = B(4)
