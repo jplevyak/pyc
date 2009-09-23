aec2dec9dfb1966aedc2ccfcad0a6f57434e3ed7
John Plevyak <jplevyak@acm.org>
2009-09-23 16:56:43 -0700

    fix add_test fix size of _CG_bool_ fix indexing

---
+++ tests/nsieve1.py
@@ -0,0 +1,22 @@
+# The Great Computer Language Shootout
+# http://shootout.alioth.debian.org/
+# nsieve benchmark for Psyco
+# Optimized from the Free Pascal version by bearophile, Jan 1 2006
+
+# import sys
+
+def nsieve(m, c=0):
+    a = [True] * (m + 1)
+    n1 = m + 1
+    for i in xrange(2, n1):
+        if a[i]:
+            c += 1
+            k = i << 1
+            while k < n1:
+                if a[k]: a[k] = False
+                k += i
+    print 'Primes up to', m, c;
+
+for k in 0, 1, 2:
+    nsieve((1 << ((10) - k)) * 10000, 0)
+
