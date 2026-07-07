4364e9ece05435f06cf15de61284b7c8b238322d
John Plevyak <jplevyak@gmail.com>
2026-07-07 13:51:06 +0200

    Fix float exponent parsing ambiguity in scanner

--- multi_inherit.py
+++ multi_inherit.py
@@ -1,18 +1,11 @@
-import sys
 class A:
-    def __init__(self):
-        self.a = int(sys.argv[1]) if len(sys.argv) > 1 else 1
     def f(self):
-        return self.a
+        return 1
 class B:
-    def __init__(self):
-        self.b = int(sys.argv[1]) + 1 if len(sys.argv) > 1 else 2
     def g(self):
-        return self.b
+        return 2
 class C(A, B):
-    def __init__(self):
-        A.__init__(self)
-        B.__init__(self)
+    pass
 c = C()
 print(c.f())
 print(c.g())
